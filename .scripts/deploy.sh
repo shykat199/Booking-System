fcda6bc6c3582dadf3b1fa56b353188788504079
shykat199 <shykatroybdku199@gmail.com>
2025-09-29 00:46:22 +0600

    fix

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -8,7 +8,7 @@ echo "Deployment started ..."
 (php artisan down) || true
 
 # Pull the latest version of the app
-git pull origin master
+git pull origin main
 
 # Install composer dependencies
 composer install --optimize-autoloader --no-dev --no-interaction
